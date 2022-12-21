d05e0222ffb57fc54c777ca15407203aa6af5fba
YovelNehmady <yovelnehmady1998@gmail.com>
2022-12-21 19:30:36 +0200

    delete map js

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,5 +1,4 @@
 import { locService } from './services/loc.service.js'
-import { mapService } from './services/map.service.js'
 
 window.onload = onInit
 window.onAddMarker = onAddMarker
@@ -8,7 +7,7 @@ window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
 
 function onInit() {
-    mapService.initMap()
+    initMap()
         .then(() => {
             console.log('Map is ready')
         })
@@ -25,7 +24,7 @@ function getPosition() {
 
 function onAddMarker() {
     console.log('Adding a marker')
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 })
+    addMarker({ lat: 32.0749831, lng: 34.9120554 })
 }
 
 function onGetLocs() {
@@ -47,7 +46,63 @@ function onGetUserPos() {
             console.log('err!!!', err)
         })
 }
+
 function onPanTo() {
     console.log('Panning the Map')
-    mapService.panTo(35.6895, 139.6917)
+    panTo(35.6895, 139.6917)
+}  
+
+                                                    ////////!MAP!///////////
+// Var that is used throughout this Module (not global)
+var gMap
+
+function initMap(lat = 32.0749831, lng = 34.9120554) {
+    return _connectGoogleApi()
+        .then(() => {
+            gMap = new google.maps.Map(
+                document.querySelector('#map'), {
+                center: { lat, lng },
+                zoom: 15
+            })
+            
+            gMap.addListener("click", (mapsMouseEvent) => {
+
+                //todo creat a new place in the loc-serivce
+                const lat = mapsMouseEvent.latLng.toJSON().lat
+                const lng = mapsMouseEvent.latLng.toJSON().lng
+                addLoc(lat,lng)
+                .then(render)
+                //todo render the prm to the locations place in the DOM
+
+            })
+            
+        })
+}
+
+function addMarker(loc) {
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!'
+    })
+    return marker
+}
+
+function panTo(lat, lng) {
+    var laLatLng = new google.maps.LatLng(lat, lng)
+    gMap.panTo(laLatLng)
+}
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    const API_KEY = 'AIzaSyCj9jbmYp9q4hOBCC3i4e1PUZjRz6KcbqY'
+    var elGoogleApi = document.createElement('script')
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
+    elGoogleApi.async = true
+    document.body.append(elGoogleApi)
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
 }
